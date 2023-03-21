c806feb50d029943bf5fce4cbecae7556cdde19e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-21 08:03:27 -0400

    add test for grep by suite name

---
+++ cypress/e2e/negative-grep-spec.js
@@ -0,0 +1,11 @@
+describe('users', () => {
+  it('works 1', () => {})
+  it('works 2', () => {})
+  it('works 3', () => {})
+})
+
+describe('projects', () => {
+  it('load 1', () => {})
+  it('load 2', () => {})
+  it('load 3', () => {})
+})
