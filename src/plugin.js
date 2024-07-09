657dd200c2c47a4f1f92a14346beff898014a60c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-09 10:14:15 -0400

    allow extra spec patterns

--- src/plugin.js
+++ src/plugin.js
@@ -7,6 +7,7 @@ const fs = require('fs')
 const path = require('path')
 const { version } = require('../package.json')
 const { parseGrep, shouldTestRun, getMentionedTags } = require('./utils')
+const { resolveFilePatterns } = require('./file-utils')
 const minimatch = require('minimatch')
 
 const MINIMATCH_OPTIONS = { dot: true, matchBase: true }
@@ -227,18 +228,18 @@ function cypressGrepPlugin(config) {
     const extraSpecsPattern = config.env.grepExtraSpecs
     if (extraSpecsPattern) {
       debug('processing the extra specs pattern "%s"', extraSpecsPattern)
-      const extraSpecsList = extraSpecsPattern
-        .split(',')
-        .map((s) => s.trim())
-        .filter(Boolean)
-      debug('extra specs list %o', extraSpecsList)
-      extraSpecsList.forEach((extraSpecPattern) => {
-        // assume for now that the pattern is a filename, not a wildcard
-        if (!greppedSpecs.includes(extraSpecPattern)) {
-          greppedSpecs.push(extraSpecPattern)
-          debug('added extra spec %s', extraSpecPattern)
+      const extraSpecs = resolveFilePatterns(extraSpecsPattern)
+      // update the config env object with resolved extra specs
+      const resolvedExtraSpecs = []
+      extraSpecs.forEach((specFilename) => {
+        if (!greppedSpecs.includes(specFilename)) {
+          greppedSpecs.push(specFilename)
+          resolvedExtraSpecs.push(specFilename)
+          debug('added extra spec %s', specFilename)
         }
       })
+
+      config.env.grepExtraSpecs = resolvedExtraSpecs
     }
 
     if (greppedSpecs.length) {
